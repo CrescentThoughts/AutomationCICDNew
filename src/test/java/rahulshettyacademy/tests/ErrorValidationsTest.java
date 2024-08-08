b2a489631549722018df7128293d101d1d035b6f
CrescentThoughts <crescentseatingsystems@gmail.com>
2024-08-08 23:18:37 +0530

    initial commit

---
+++ src/test/java/rahulshettyacademy/tests/ErrorValidationsTest.java
@@ -0,0 +1,45 @@
+package rahulshettyacademy.tests;
+
+import java.io.IOException;
+import java.util.List;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import rahulshettyacademy.TestComponents.BaseTest;
+import rahulshettyacademy.TestComponents.Retry;
+import rahulshettyacademy.pageobjects.CartPage;
+import rahulshettyacademy.pageobjects.ProductCatalogue;
+
+public class ErrorValidationsTest extends BaseTest {
+
+	@Test(groups= {"ErrorHandling"},retryAnalyzer=Retry.class)
+	public void LoginErrorValidation() throws IOException, InterruptedException {
+
+	
+		landingPage.loginApplication("anshika@gmail.com", "Iamki000");
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+
+	}
+	
+
+	@Test
+	public void ProductErrorValidation() throws IOException, InterruptedException
+	{
+
+		String productName = "ZARA COAT 3";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("rahulshetty@gmail.com", "Iamking@000");
+		List<WebElement> products = productCatalogue.getProductList();
+		System.out.println("Products list is empty - "+products.isEmpty()); //removing warnings for 'products' variable not used
+		productCatalogue.addProductToCart(productName);
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+		Assert.assertFalse(match);
+		
+	
+
+	}
+
+	
+	
+
+}
